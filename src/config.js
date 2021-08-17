a09351f3bdf78a3490c4b27ee8df99da9a6c6f5f
Pascal Barth <pak.barth@gmail.com>
2021-08-17 16:55:46 +0200

    Forcing data.geo.admin.ch as the default catalog provider

--- src/config.js
+++ src/config.js
@@ -1,17 +1,12 @@
 export const CATALOG_URL =
-  process.env.CATALOG_URL ||
-  "https://raw.githubusercontent.com/cholmes/sample-stac/master/stac/catalog.json";
+  process.env.CATALOG_URL || "https://data.geo.admin.ch/api/stac/v0.9/";
 
-export const STAC_VERSION =
-  process.env.STAC_VERSION ||
-  "0.9.0";
+export const STAC_VERSION = process.env.STAC_VERSION || "0.9.0";
 
 export const TILE_SOURCE_TEMPLATE =
   process.env.TILE_SOURCE_TEMPLATE ||
   "https://tiles.rdnt.io/tiles/{z}/{x}/{y}@2x?url={ASSET_HREF}";
 
-export const STAC_PROXY_URL =
-  process.env.STAC_PROXY_URL;
+export const STAC_PROXY_URL = process.env.STAC_PROXY_URL;
 
-export const TILE_PROXY_URL =
-  process.env.TILE_PROXY_URL
+export const TILE_PROXY_URL = process.env.TILE_PROXY_URL;
